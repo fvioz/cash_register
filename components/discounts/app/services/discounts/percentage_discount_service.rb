af69a8ed69a2c13db7abb3acdcab806db07da70a
Fabian Sabogal <me@fvioz.dev>
2025-09-05 14:20:41 +0200

    Add for applying percentage-based discounts

---
+++ components/discounts/app/services/discounts/percentage_discount_service.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+# typed: true
+
+module Discounts
+  class PercentageDiscountService
+    extend T::Sig
+
+    include Concerns::Discountable
+
+    PercentageType = T.type_alias { T.any(Integer, Float, BigDecimal) }
+
+    sig { params(threshold: Integer, percentage: PercentageType).void }
+    def initialize(threshold, percentage)
+      super()
+      @threshold = threshold
+      @percentage = percentage
+
+      return unless @percentage.negative? || @percentage > 100
+
+      raise ArgumentError, 'Percentage must be between 0 and 100'
+    end
+
+    sig { override.params(price: BigDecimal, quantity: Integer).returns(DiscountCalculationType) }
+    def calculate(price, quantity)
+      {
+        total_discount: total_discount(price, quantity),
+        total_price: total_price(price, quantity)
+      }
+    end
+
+    private
+
+    sig { params(quantity: Integer).returns(T::Boolean) }
+    def exceeds_threshold?(quantity)
+      quantity >= @threshold
+    end
+
+    sig { params(price: BigDecimal, quantity: Integer).returns(BigDecimal) }
+    def total_discount(price, quantity)
+      return BigDecimal('0.0') unless exceeds_threshold?(quantity)
+
+      (price * quantity) - total_price(price, quantity)
+    end
+
+    sig { params(price: BigDecimal, quantity: Integer).returns(BigDecimal) }
+    def total_price(price, quantity)
+      return price * quantity unless exceeds_threshold?(quantity)
+
+      (price - ((price * @percentage) / 100)) * quantity
+    end
+  end
+end
---
+++ components/discounts/spec/services/discounts/percentage_discount_service_spec.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+# typed: false
+
+RSpec.describe Discounts::PercentageDiscountService, type: :service do
+  subject(:service) { described_class.new(threshold, percentage) }
+
+  let(:price) { BigDecimal(2) }
+  let(:quantity) { 3 }
+  let(:threshold) { 3 }
+  let(:percentage) { 20 }
+
+  describe '#calculate' do
+    let(:result) { service.calculate(price, quantity) }
+
+    context 'when percentage is out of range' do
+      describe 'less than 0' do
+        let(:percentage) { BigDecimal('-0.0000001') }
+
+        it 'raises an error' do
+          expect { service }.to raise_error(ArgumentError, 'Percentage must be between 0 and 100')
+        end
+      end
+
+      describe 'bigger than 100' do
+        let(:percentage) { BigDecimal('100.0000001') }
+
+        it 'raises an error' do
+          expect { service }.to raise_error(ArgumentError, 'Percentage must be between 0 and 100')
+        end
+      end
+    end
+
+    context 'when quantity is 0' do
+      let(:quantity) { 0 }
+
+      it 'returns zero total total price' do
+        expect(result[:total_price]).to eq(BigDecimal(0))
+      end
+
+      it 'returns zero total discount' do
+        expect(result[:total_discount]).to eq(BigDecimal(0))
+      end
+    end
+
+    context 'when quantity does not exceed the threshold' do
+      let(:quantity) { 2 }
+
+      it 'returns the correct total total price' do
+        expect(result[:total_price]).to eq(BigDecimal(4))
+      end
+
+      it 'returns the correct total discount' do
+        expect(result[:total_discount]).to eq(BigDecimal(0))
+      end
+    end
+
+    context 'when quantity exceeds the threshold' do
+      let(:quantity) { 3 }
+
+      it 'returns the correct total total price' do
+        expect(result[:total_price]).to eq(BigDecimal('4.8'))
+      end
+
+      it 'returns the correct total discount' do
+        expect(result[:total_discount]).to eq(BigDecimal('1.2'))
+      end
+    end
+  end
+end
