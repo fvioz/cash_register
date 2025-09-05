14e1dba3c7f1625970149f982a2b133a6fdc9a77
Fabian Sabogal <me@fvioz.dev>
2025-09-05 13:03:28 +0200

    Add sorbet files

---
+++ sorbet/tapioca/require.rb
@@ -0,0 +1,10 @@
+# typed: true
+# frozen_string_literal: true
+
+require 'bigdecimal'
+require 'bigdecimal/util'
+require 'cli/ui'
+require 'debug'
+require 'rspec'
+require 'simplecov'
+require 'zeitwerk'
