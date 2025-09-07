afbd1d75405523020bed298c0bed1c0e9f304014
Fabian Sabogal <me@fvioz.dev>
2025-09-07 11:21:29 +0200

    Add  command to process basket items
    - Introduce a new  command in the CLI to handle product codes separated by commas.
    - Create a new file  to encapsulate the logic for processing scanned items.

--- app/cli/checkout.rb
+++ app/cli/checkout.rb
@@ -9,6 +9,11 @@ module Cli
       true
     end
 
+    desc 'scan BASKET_ITEMS', 'Product code name separated by comma without spaces i.e GR1,SR1,CF1'
+    def scan(basket_items)
+      Commands::Scan.new(basket_items.split(',')).call
+    end
+
     desc 'version', 'Show application version'
     def version
       Commands::Version.call
---
+++ app/cli/commands/scan.rb
@@ -0,0 +1,75 @@
+# frozen_string_literal: true
+# typed: true
+
+module Cli
+  module Commands
+    class Scan
+      extend T::Sig
+
+      sig { params(basket_items: T::Array[String], pricing_rules: Discounts::PricingRulesService, currency: String).void }
+      def initialize(basket_items, pricing_rules = Discounts::PricingRulesService.new, currency = 'EUR')
+        @basket_items = basket_items
+        @currency = currency
+        @checkout = Checkouts::CheckoutService.new(pricing_rules)
+      end
+
+      sig { void }
+      def call
+        @basket_items.each do |item|
+          @checkout.scan(item.strip)
+        rescue StandardError
+          puts "Product with code #{item.strip} not found."
+        end
+
+        print_receipt
+      end
+
+      sig { void }
+      def print_receipt
+        frame 'Checkout' do
+          @checkout.cart.each do |item|
+            prrint_item_total(item)
+          end
+
+          CLI::UI::Frame.divider('')
+          subtotal
+
+          CLI::UI::Frame.divider('{{v}}')
+          total
+        end
+      end
+
+      sig { params(item: Checkouts::CheckoutItemService).void }
+      def prrint_item_total(item)
+        item_original_total = format_price(item.original_total_price)
+        item_total = format_price(item.total_price)
+
+        original_total_string = item_total == item_original_total ? '' : "{{strikethrough:#{item_original_total}}} "
+
+        puts CLI::UI.fmt "#{item.product.name} (x#{item.quantity}): #{original_total_string}#{item_total}"
+      end
+
+      sig { void }
+      def subtotal
+        puts CLI::UI.fmt "Total without discount: {{blue:#{format_price(@checkout.original_total)}}}"
+        puts CLI::UI.fmt "Total discount: {{blue:#{format_price(@checkout.discount)}}}"
+      end
+
+      sig { void }
+      def total
+        puts CLI::UI.fmt "{{bold:Total price:}} {{green:#{format_price(@checkout.total)}}}"
+      end
+
+      sig { params(name: String, block: T.untyped).void }
+      def frame(name, &block)
+        CLI::UI::StdoutRouter.enable
+        CLI::UI::Frame.open(name, &block)
+      end
+
+      sig { params(amount: BigDecimal).returns(String) }
+      def format_price(amount)
+        Money.from_amount(amount, @currency).format
+      end
+    end
+  end
+end
